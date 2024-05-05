0acb6f73e2862eeca29b925fb84437c1507b1403
TEJU D <111564230+tejudharanesh@users.noreply.github.com>
2024-05-05 21:38:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { books } from "./books";
 import Book from "./Book";
 
 function App() {
+
   return (
     <>
       <h1>My collections</h1>
       <section className="booklist">
         {books.map((book, index) => {
           return <Book {...book} key={book.id} number={index} />;
-        })}
+        })
+}
       </section>
     </>
   );
